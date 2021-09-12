58d98181350f7aab04dafec6cce79192ff1eabe6
Timoh97 <timothy.nthiga@student.moringaschool.com>
2021-09-12 16:46:07 +0300

    add a pop up upon submitting mailchimp details

--- scripts.js
+++ scripts.js
@@ -3,35 +3,23 @@ $(document).ready(function(){
 
 $("#img-design").click(function(){
     $("#img-descr").toggle()
-    $("#img-design").hide()
 })
 
 $("#img-dev").click(function(){
     $("#dev-descr").toggle()
-    $("#img-dev").hide()
 })
 
 $("#img-service").click(function(){
     $("#service-descr").toggle()
-    $("#img-service").hide()
-})
-
 })
 
 
 
 
-function subscriptionChimp(){
-    var fName = document.getElementById("mce-FNAME").value;
-    var email = document.getElementById("mce-EMAIL").value;
-    var message = document.getElementById("mce-YOURMESSAG").value;
+$("#mc-embedded-subscribe").click(function() {
+    var fName = $("input#mce-FNAME").val()
 
-    if (isNaN(fNameame) && isNaN(email)) {
-        alert (fName + "we have received your message. Thank you for reaching out to us.")
-    }
+    alert (fName + " we have received your message. Thank you for reaching out to us.");
+})
 
-
-
-}
-
-// alert(fName + "we have received your message. Thank you fro reaching out to us. ")
+})
