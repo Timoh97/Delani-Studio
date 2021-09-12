2c9ecb7f7f057eedb5b95c9d8c0facbbf8525273
Timoh97 <timothy.nthiga@student.moringaschool.com>
2021-09-12 10:18:38 +0300

    add toggle effects

---
+++ scripts.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+
+
+$("#img-design").click(function(){
+    $("#img-descr").toggle()
+    $("#img-design").show()
+})
+
+$("#img-dev").click(function(){
+    $("#dev-descr").toggle()
+    $("#img-dev").show()
+})
+
+$("#img-service").click(function(){
+    $("#service-descr").toggle()
+    $("#img-service").sow()
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
