309e58850c0f79e15311d42387f4f583d3b25aed
Timoh97 <timothy.nthiga@student.moringaschool.com>
2021-09-12 14:18:40 +0300

    add mailchimp alert message

--- scripts.js
+++ scripts.js
@@ -16,10 +16,22 @@ $("#img-service").click(function(){
     $("#img-service").hide()
 })
 
-
-
-
-
 })
 
 
+
+
+function subscriptionChimp(){
+    var fName = document.getElementById("mce-FNAME").value;
+    var email = document.getElementById("mce-EMAIL").value;
+    var message = document.getElementById("mce-YOURMESSAG").value;
+
+    if (isNaN(fNameame) && isNaN(email)) {
+        alert (fName + "we have received your message. Thank you for reaching out to us.")
+    }
+
+
+
+}
+
+// alert(fName + "we have received your message. Thank you fro reaching out to us. ")
